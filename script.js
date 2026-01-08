4dd4cdc7ea873b26f6fc2a3c83a3230cbb3c411c
scandale191-debug <scandale191@gmail.com>
2026-01-08 11:24:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const slides = document.querySelectorAll(".slide");
-let current = 0;
 
-setInterval(() => {
-  slides[current].classList.remove("active");
-  current = (current + 1) % slides.length;
-  slides[current].classList.add("active");
-}, 4000);
