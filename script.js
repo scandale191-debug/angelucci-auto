94a17183b2d33422f2003d39111ab2f05cb782c5
scandale191-debug <scandale191@gmail.com>
2026-01-09 18:30:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-let slideIndex = 0;
-const slides = document.querySelectorAll(".slide");
+let index=0;
+const slides=document.querySelectorAll(".slide");
 
 function showSlide(i){
 slides.forEach(s=>s.classList.remove("active"));
 slides[i].classList.add("active");
 }
 function nextSlide(){
-slideIndex=(slideIndex+1)%slides.length;
-showSlide(slideIndex);
+index=(index+1)%slides.length;
+showSlide(index);
 }
 function prevSlide(){
-slideIndex=(slideIndex-1+slides.length)%slides.length;
-showSlide(slideIndex);
+index=(index-1+slides.length)%slides.length;
+showSlide(index);
 }
 setInterval(nextSlide,5000);
 
 const track=document.querySelector(".featured-track");
 function scrollFeatured(dir){
-track.scrollBy({left:dir*500,behavior:"smooth"});
+track.scrollBy({left:dir*450,behavior:"smooth"});
 }
+
