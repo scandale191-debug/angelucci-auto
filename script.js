f0b01151deb0e620e0e00cdf7c604dcda9df6f04
scandale191-debug <scandale191@gmail.com>
2026-01-09 12:59:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let index = 0;
 const slides = document.querySelectorAll('.slide');
 
-function changeSlide(step) {
+function changeSlide(step){
     slides[index].classList.remove('active');
     index = (index + step + slides.length) % slides.length;
     slides[index].classList.add('active');
 }
 
-setInterval(() => changeSlide(1), 6000);
+setInterval(()=>changeSlide(1),7000);
